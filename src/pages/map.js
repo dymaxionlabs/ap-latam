4cadac2f57f7cd1f3e40eb98a058656b393ce9d1
Damián Silvani <munshkr@gmail.com>
2018-02-13 11:50:06 -0300

    Automatic resize on Map

--- src/pages/map.js
+++ src/pages/map.js
@@ -5,10 +5,9 @@ import css from './map.sass'
 class Map extends React.Component {
   constructor(props) {
     super(props)
+
     this.state = {
       viewport: {
-        width: 400,
-        height: 400,
         latitude: this.props.lat,
         longitude: this.props.lon,
         zoom: this.props.zoom
@@ -16,10 +15,32 @@ class Map extends React.Component {
     }
   }
 
+  componentDidMount() {
+    window.addEventListener('resize', this._resize);
+    this._resize();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this._resize);
+  }
+
+  _onViewportChange = viewport => this.setState({
+    viewport: {...this.state.viewport, ...viewport}
+  });
+
+  _resize = () => this._onViewportChange({
+    width: this.props.width || window.innerWidth,
+    height: this.props.height || window.innerHeight
+  });
+
   render() {
+    const {viewport, settings} = this.state
     return <ReactMapGL
-      {...this.state.viewport}
-      onViewportChange={(viewport) => this.setState({viewport})}
+      {...viewport}
+      {...settings}
+      id="map"
+      mapStyle="mapbox://styles/mapbox/satellite-streets-v9"
+      onViewportChange={this._onViewportChange}
     />
   }
 }
@@ -27,8 +48,10 @@ class Map extends React.Component {
 const MapPage = ({ data }) => {
   const cityName = 'Buenos Aires'
   const countryName = 'Argentina'
-  const city = data.allCitiesJson.edges.find(e => e.node.name == cityName && e.node.country == countryName).node
+  const city = data.allCitiesJson.edges
+    .find(e => e.node.name == cityName && e.node.country == countryName).node
   console.log(city)
+
   return <Map
     zoom={city.zoom}
     lat={city.center.lat}
