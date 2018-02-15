d34aff3794968b0337cea7e3210e29cdf8cd17c8
Damián Silvani <munshkr@gmail.com>
2018-02-15 18:16:04 -0300

    Add gatsby-plugin-i18n

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,5 +40,12 @@ module.exports = {
         anonymize: true,
       },
     },
+    {
+      resolve: 'gatsby-plugin-i18n',
+      options: {
+        langKeyDefault: 'es',
+        useLangKeyLayout: false
+      }
+    }
   ],
 };
