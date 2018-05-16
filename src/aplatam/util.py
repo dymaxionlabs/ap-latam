32981a8ad1cb416627ed91e833ea489a2e25a925
Gessica <gessica@dymaxionlabs.com>
2018-05-16 12:25:56 -0300

    Replace iglob with glob

--- src/aplatam/util.py
+++ src/aplatam/util.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
-from glob import iglob
-
+from glob import glob
+import os
+import pdb
 def all_raster_files(dirname, ext='.tif'):
     """Generate any raster files inside +dirname+, recursively"""
-    pattern = '**/*.{ext}'.format(ext)
-    return iglob(os.path.join(dirname, pattern), recursive=True)
+    pattern = '**/*{ext}'.format(ext=ext)
+    return glob(os.path.join(dirname, pattern), recursive=True)
+    
+
