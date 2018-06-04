0909ce1e1268004597c4ed668fe2de7edb381519
Damián Silvani <munshkr@gmail.com>
2018-06-04 13:40:09 -0300

    Write basic integration test for ap_prepare
    Include a Sentinel-2 clipped image and vector file as fixtures.

--- tests/console/test_prepare.py
+++ tests/console/test_prepare.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import json
+import os
+import tempfile
+
 import pytest
 from mock import call, patch
 
+from aplatam import __version__
+from aplatam.build_trainset import CnnTrainsetBuilder
 from aplatam.console.prepare import validate_rasters_band_count
 
 
@@ -34,3 +40,20 @@ def test_validate_rasters_band_count_fail(mock_func, some_rasters):
     with pytest.raises(RuntimeError):
         assert validate_rasters_band_count(some_rasters)
     mock_func.assert_has_calls([call('a.tif'), call('b.tif'), call('c.tif')])
+
+
+def test_cnn_trainset_builder():
+    builder = CnnTrainsetBuilder(
+        ['tests/fixtures/sen2_20161215_clipped.tif'],
+        'tests/fixtures/settlements.geojson',
+        size=128,
+        step_size=64)
+
+    with tempfile.TemporaryDirectory(prefix='aplatam_test_') as tmpdir:
+        builder.build(tmpdir)
+
+        metadata_path = os.path.join(tmpdir, 'metadata.json')
+        with open(metadata_path) as f:
+            metadata = json.load(f)
+            assert isinstance(metadata, dict)
+            assert metadata['version'] == __version__
