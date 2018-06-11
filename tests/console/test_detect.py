37f2425aebb377bd8d933a9eaf5570ca541b3367
Damián Silvani <munshkr@gmail.com>
2018-06-11 12:26:39 -0300

    Add tests for console scripts

--- tests/console/test_detect.py
+++ tests/console/test_detect.py
@@ -1,1 +1,25 @@
+import os
+import tempfile
+
+from mock import patch
+
 import aplatam.console.detect as ap_detect
+
+
+@patch('aplatam.console.detect.detect')
+def test_run_script_default_arguments(train_mock_func):
+    with tempfile.TemporaryDirectory(prefix='ap_detect') as tmpdir:
+        output_geojson = os.path.join(tmpdir, 'detection.geojson')
+
+        ap_detect.main(
+            ['tests/fixtures/model.h5', 'tests/fixtures/', output_geojson])
+
+        train_mock_func.assert_called_once_with(
+            input_dir='tests/fixtures/',
+            mean_threshold=0.3,
+            model_file='tests/fixtures/model.h5',
+            neighbours=3,
+            output=output_geojson,
+            rescale_intensity=True,
+            step_size=None,
+            threshold=0.3)
--- tests/console/test_train.py
+++ tests/console/test_train.py
@@ -39,7 +39,7 @@ def test_validate_rasters_band_count_fail(mock_func, some_rasters):
     mock_func.assert_has_calls([call('a.tif'), call('b.tif')])
 
 
-@pytest.mark.skip(reason="issues with TravisCI and tensorflow-gpu")
+#@pytest.mark.skip(reason="issues with TravisCI and tensorflow-gpu")
 @patch('aplatam.console.train.train')
 def test_run_script_default_arguments(train_mock_func):
     with tempfile.TemporaryDirectory(prefix='ap_train') as tmpdir:
@@ -47,12 +47,10 @@ def test_run_script_default_arguments(train_mock_func):
             ['tests/fixtures/', 'tests/fixtures/settlements.geojson', tmpdir])
 
         output_model_path = os.path.join(tmpdir, 'model.h5')
-        # FIXME mock assert not working
-        assert True
-        # assert train_mock_func.assert_called_once_with(
-        #     output_model_path,
-        #     tmpdir,
-        #     trainable_layers=5,
-        #     batch_size=5,
-        #     epochs=20,
-        #     size=256)
+        train_mock_func.assert_called_once_with(
+            output_model_path,
+            tmpdir,
+            trainable_layers=5,
+            batch_size=5,
+            epochs=20,
+            size=256)
