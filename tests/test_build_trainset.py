0964635d1cf9411317c1003213e5dd53c0aae938
Gessica <gessica@dymaxionlabs.com>
2018-06-15 18:21:29 -0300

    Add more tests for CnnTrainsetBuilder

--- tests/test_build_trainset.py
+++ tests/test_build_trainset.py
@@ -17,23 +17,54 @@ def test_cnn_trainset_builder():
     with tempfile.TemporaryDirectory(
             prefix='aplatam_test_build_trainset') as tmpdir:
         builder.build(tmpdir)
+        assert_trainset(tmpdir, builder)
 
-        metadata_path = os.path.join(tmpdir, 'metadata.json')
-        assert os.path.exists(metadata_path)
-        with open(metadata_path) as f:
-            metadata = json.load(f)
-            assert isinstance(metadata, dict)
-            assert metadata["version"] == __version__
-            list_argument = [
-                'size', 'step_size', 'buffer_size', 'rescale_intensity',
-                'lower_cut', 'upper_cut'
-            ]
-            for name_argument in list_argument:
-                assert metadata[name_argument] == getattr(
-                    builder, name_argument)
 
-        for dir_a, dir_b in zip(('train', 'validation', 'test'), ('t', 'f')):
-            dirname = os.path.join(tmpdir, dir_a, dir_b)
-            assert os.path.exists(dirname), '{} exists'.format(dirname)
+def test_cnn_trainset_builder_with_buffer_size():
+    builder = CnnTrainsetBuilder(
+        ['tests/fixtures/sen2_20161215_clipped.tif'],
+        'tests/fixtures/settlements.geojson',
+        size=128,
+        step_size=64,
+        buffer_size=5)
+
+    with tempfile.TemporaryDirectory(
+            prefix='aplatam_test_build_trainset') as tmpdir:
+        builder.build(tmpdir)
+        assert_trainset(tmpdir, builder)
+
+
+def test_cnn_trainset_builder_with_no_rescale_intensity():
+    builder = CnnTrainsetBuilder(
+        ['tests/fixtures/sen2_20161215_clipped.tif'],
+        'tests/fixtures/settlements.geojson',
+        size=128,
+        step_size=64,
+        rescale_intensity=False)
+
+    with tempfile.TemporaryDirectory(
+            prefix='aplatam_test_build_trainset') as tmpdir:
+        builder.build(tmpdir)
+        assert_trainset(tmpdir, builder, empty_dirs=True)
+
+
+def assert_trainset(tmpdir, builder, empty_dirs=False):
+    metadata_path = os.path.join(tmpdir, 'metadata.json')
+    assert os.path.exists(metadata_path)
+    with open(metadata_path) as f:
+        metadata = json.load(f)
+        assert isinstance(metadata, dict)
+        assert metadata["version"] == __version__
+        list_argument = [
+            'size', 'step_size', 'buffer_size', 'rescale_intensity',
+            'lower_cut', 'upper_cut'
+        ]
+        for name_argument in list_argument:
+            assert metadata[name_argument] == getattr(builder, name_argument)
+
+    for dir_a, dir_b in zip(('train', 'validation', 'test'), ('t', 'f')):
+        dirname = os.path.join(tmpdir, dir_a, dir_b)
+        assert os.path.exists(dirname), '{} exists'.format(dirname)
+        if not empty_dirs:
             assert glob.glob(os.path.join(
                 dirname, '*.jpg')), '{} contains at least one jpg file'
