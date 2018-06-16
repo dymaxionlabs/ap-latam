c73f15d48dc6c11533909d7afb01dc2f34d42df7
Damián Silvani <munshkr@gmail.com>
2018-06-16 13:27:43 -0300

    Add class_weight option on model fit call

--- aplatam/train_classifier.py
+++ aplatam/train_classifier.py
@@ -19,9 +19,13 @@ def train(output_model_file, dataset_dir, *, trainable_layers, batch_size,
     train_data_dir = os.path.join(dataset_dir, 'train')
     validation_data_dir = os.path.join(dataset_dir, 'validation')
 
-    train_files = glob(os.path.join(train_data_dir, '**', '*.jpg'))
+    true_train_files = glob(os.path.join(train_data_dir, 't', '*.jpg'))
+    false_train_files = glob(os.path.join(train_data_dir, 'f', '*.jpg'))
+    train_files = true_train_files + false_train_files
     validation_files = glob(os.path.join(validation_data_dir, '**', '*.jpg'))
 
+    nb_true_train_samples = len(true_train_files)
+    nb_false_train_samples = len(false_train_files)
     nb_train_samples = len(train_files)
     nb_validation_samples = len(validation_files)
 
@@ -29,6 +33,11 @@ def train(output_model_file, dataset_dir, *, trainable_layers, batch_size,
 
     assert size >= 197, \
         'image size must be at least 197x197, but was {size}x{size}'.format(size=size)
+
+    class_weight = {
+        0: 1.,
+        1: round(nb_false_train_samples / nb_true_train_samples)}
+    _logger.info('Class weight: %s', class_weight)
 
     # Build model using ResNet-50 as base input
     base_model = build_resnet50_model(img_width, img_height)
@@ -38,8 +47,9 @@ def train(output_model_file, dataset_dir, *, trainable_layers, batch_size,
     compile_model(model)
 
     # Prepare data generators for training and test sets
-    train_datagen = ImageDataGenerator(rescale=1. / 255, horizontal_flip=True)
-    test_datagen = ImageDataGenerator(rescale=1. / 255, horizontal_flip=True)
+    opts = dict(rescale=1. / 255, horizontal_flip=True)
+    train_datagen = ImageDataGenerator(**opts)
+    test_datagen = ImageDataGenerator(**opts)
 
     train_generator = train_data_generator(train_datagen, train_data_dir,
                                            img_height, img_width, batch_size)
@@ -57,6 +67,7 @@ def train(output_model_file, dataset_dir, *, trainable_layers, batch_size,
         train_samples=nb_train_samples,
         validation_generator=validation_generator,
         validation_samples=nb_validation_samples,
+        class_weight=class_weight,
         batch_size=batch_size,
         epochs=epochs,
         early_stopping=early_stopping)
@@ -75,13 +86,14 @@ def build_resnet50_model(img_width, img_height):
         input_shape=(img_width, img_height, 3))
 
 
-def train_model(model, *, train_generator, validation_generator, train_samples,
+def train_model(model, *, train_generator, validation_generator, train_samples, class_weight,
                 validation_samples, batch_size, epochs, early_stopping):
     """Train model"""
     model.fit_generator(
         train_generator,
         steps_per_epoch=train_samples // batch_size,
         epochs=epochs,
+        class_weight=class_weight,
         validation_data=validation_generator,
         validation_steps=validation_samples // batch_size,
         callbacks=[early_stopping])
@@ -118,12 +130,12 @@ def compile_model(model):
 
 def add_custom_layers(model):
     """Adding custom Layers"""
-    model = model.output
-    model = Flatten()(model)
-    model = Dense(1024, activation='relu')(model)
-    model = Dropout(0.5)(model)
-    model = Dense(1, activation='sigmoid')(model)
-    return model
+    out = model.output
+    out = Flatten()(out)
+    out = Dense(1024, activation='relu')(out)
+    out = Dropout(0.5)(out)
+    out = Dense(1, activation='sigmoid')(out)
+    return out
 
 
 def freeze_layers(model, trainable_layers):
